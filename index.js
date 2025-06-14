d0bef42c18428176238a2aed1cfea7a525b5eebf
transMenschen <transmenschen.de@gmail.com>
2025-06-14 16:48:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ client.on("messageCreate", async message => {
 
   try {
     const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: "gpt-4o",
       messages: [
         {
           role: "system",
